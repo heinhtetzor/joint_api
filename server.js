49c85356619698ba3f174198f5baef433dcf8728
Hein Htet Zaw <heinhzor@gmail.com>
2020-03-01 14:51:57 +0800

    update server.js - bodyparser
    Bodyparser has become part of express.js in express 4.x. So dont need to install body-parser anymore.

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ if (mongoose.connection.readyState != 1) {
 }
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 authRoutes(app);
 userRoutes(app);
